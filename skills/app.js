df310a29ba548faa299b2174eacb230ac34728cf
Jay <35671@ma-web.nl>
2024-03-14 14:50:34 +0100

    skills opdracht

---
+++ skills/app.js
@@ -0,0 +1,66 @@
+const startButton = document.getElementById("js--start");
+const stopButton = document.getElementById("js--stop");
+const resetButton = document.getElementById("js--reset");
+let seconds = 0;
+let minutes = 0;
+let running = false;
+
+const secondsTimer = document.getElementById("js--secondsTimer")
+const minutesTimer = document.getElementById("js--minutesTimer")
+const minisecondsTimer = document.getElementById("js--minisecondsTimer")
+
+let timer;
+
+startButton.onclick = function(){
+    if(running === true){
+        return;
+    }
+
+    running = true;
+    timer = setInterval(function(){
+        seconds += 1;
+        if(seconds === 60){
+            minutes += 1;
+            minutesTimer.innerText = minutes;
+            seconds = 0;
+        }
+        secondsTimer.innerText = seconds; 
+    }, 1000);
+}
+
+stopButton.onclick = function(){
+    clearInterval(timer);
+    running = false;
+}
+
+resetButton.onclick = function(){
+    seconds = 0;
+    minutes = 0;
+    minutesTimer.innerText = 0;
+    secondsTimer.innerText = 0;
+}
+
+const rangeValue = document.getElementById("js--rangeValue");
+const slider = document.getElementById("js--slider");
+const body = document.getElementById("js--body");
+slider.value = 2;
+rangeValue.innerText = slider.value + "x";
+
+slider.oninput = function() {
+    rangeValue.innerText = slider.value + "x";
+    body.style.fontSize = slider.value + "rem";
+}
+
+const img = document.getElementById("js--img");
+const paragraph = document.getElementById("js--text")
+
+let data = fetch("data.json").then(
+    function(binnenGekomenData){
+        return binnenGekomenData.json();
+    }).then(
+        function(echteData){
+            paragraph.innerHTML = echteData.text;
+            img.setAttribute("src", echteData.img)
+        }
+    )
+
